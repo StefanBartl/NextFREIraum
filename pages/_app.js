fe51698f680baee53ee933bd3705a3a045f52221
Steve B <wkdatabase@gmail.com>
2022-08-17 00:41:02 +0200

    Setup SCSS Files

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import '../styles/globals.scss';
-import 'bootstrap-icons/font/bootstrap-icons.css'
-import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function MyApp({ Component, pageProps }) {
   return (<Component {...pageProps}/>)
